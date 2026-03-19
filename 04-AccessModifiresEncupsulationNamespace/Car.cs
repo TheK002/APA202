28f6b794938aa6d2b51ee4f9fc80c0de64812c6d
TheK002 <zakaeh-apa202@code.edu.az>
2026-03-19 16:30:37 +0400

    Add files via upload

---
+++ 04-AccessModifiresEncupsulationNamespace/Car.cs
@@ -0,0 +1,79 @@
+﻿public class Car : Vehicle
+{
+    private double _fuelCapacity;
+    private double _fuel;
+
+    public double FuelCapacity
+    {
+        get => _fuelCapacity;
+        set
+        {
+            if (value <= 0)
+                throw new ArgumentException("FuelCapacity müsbət olmalıdır.");
+            _fuelCapacity = value;
+        }
+    }
+
+    public double Fuel
+    {
+        get => _fuel;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Fuel mənfi ola bilməz.");
+            if (value > FuelCapacity)
+                throw new ArgumentException("Fuel, FuelCapacity-dən böyük ola bilməz.");
+            _fuel = value;
+        }
+    }
+
+    public double FuelConsumptionPer100Km { get; set; }
+
+    public Car(string brand, string model, int year,
+               double fuelCapacity,
+               double fuelConsumptionPer100Km,
+               double fuel)
+        : base(brand, model, year)
+    {
+        FuelCapacity = fuelCapacity;
+        FuelConsumptionPer100Km = fuelConsumptionPer100Km;
+        Fuel = fuel;
+    }
+
+    public void Refuel(double liters)
+    {
+        if (Fuel + liters > FuelCapacity)
+        {
+            Console.WriteLine("Çox yanacaq doldurmaq olmaz!");
+            return;
+        }
+
+        Fuel += liters;
+    }
+
+    public override void Drive(int km)
+    {
+        if (!IsRunning)
+        {
+            Console.WriteLine("Mühərrik işləmədən sürmək olmaz!");
+            return;
+        }
+
+        double requiredLiters = (km / 100.0) * FuelConsumptionPer100Km;
+
+        if (Fuel < requiredLiters)
+        {
+            Console.WriteLine("Kifayət qədər yanacaq yoxdur!");
+            return;
+        }
+
+        Fuel -= requiredLiters;
+        MileageKm += km;
+    }
+
+    public override void VehicleInfo()
+    {
+        base.VehicleInfo();
+        Console.WriteLine($"Fuel: {Fuel}L / {FuelCapacity}L");
+    }
+}
---
+++ 04-AccessModifiresEncupsulationNamespace/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Car car = new Car("Toyota", "Corolla", 2018, 50, 7.5, 20);
+
+        car.StartEngine();
+        car.Drive(100);
+        car.VehicleInfo();
+
+        car.Refuel(10);
+        car.VehicleInfo();
+    }
+}
