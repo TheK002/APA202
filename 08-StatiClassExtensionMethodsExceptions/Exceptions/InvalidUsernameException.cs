9a1d457afb639773def3e9d8fdb75a3d33ecda72
TheK002 <zakaeh-apa202@code.edu.az>
2026-03-30 22:45:53 +0400

    task8

---
+++ 08-StatiClassExtensionMethodsExceptions/Exceptions/InvalidUsernameException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08_StatiClassExtensionMethodsExceptions.Exceptions
+{
+    public class InvalidUsernameException : Exception
+    {
+        public InvalidUsernameException()
+            : base("Username is invalid!") { }
+
+        public InvalidUsernameException(string message)
+            : base(message) { }
+    }
+}
---
+++ 08-StatiClassExtensionMethodsExceptions/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08_StatiClassExtensionMethodsExceptions.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException()
+            : base("User not found!") { }
+
+        public UserNotFoundException(string username)
+            : base($"User '{username}' not found!") { }
+    }
+}
