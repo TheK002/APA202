ab4dd6b4c7ad0d332c613f7b57f2c79a7f8485a5
zeka <zeka.huseynliwork@gmail.com>
2026-04-09 23:08:29 +0400

    half of the methods done

---
+++ OneDrive/Desktop/15-ConsoleApp/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace _15_ConsoleApp.Models
+{
+    internal class Student
+    {
+        private static int _id = 0;
+        public int Id { get; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public CourseGroup CourseGroup { get; set; }
+
+        public Student(string name, string surname, int age)
+        {
+            _id++;
+            Id = _id;
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+    }
+}
---
+++ OneDrive/Desktop/15-ConsoleApp/Service/CourseService.cs
@@ -0,0 +1,61 @@
+﻿using _15_ConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace _15_ConsoleApp.Service
+{
+    internal class CourseService
+    {
+        private List<CourseGroup> groups = new();
+        private List<Student> students = new();
+
+        public void CreateGroup(CourseGroup group)
+        {
+            groups.Add(group);
+        }
+
+        public void UpdateGroup(int id, string name, string teacher, int room)
+        {
+            var group = groups.FirstOrDefault(x => x.Id == id);
+            if (group != null)
+            {
+                group.Name = name;
+                group.Teacher = teacher;
+                group.Room = room;
+            }
+
+        }
+
+        public void DeleteGroup(int id)
+        {
+            var group = groups.FirstOrDefault(x => x.Id == id);
+            if (group != null)
+            {
+                groups.Remove(group);
+            }
+        }
+
+        public CourseGroup GetGroupById(int id)
+        {
+             return groups.FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<CourseGroup> GetGroupByTeacher(string teacher)
+        {
+            return groups.Where(x => x.Teacher == teacher).ToList();
+        }
+
+        public List<CourseGroup> GetGroupByRoom(int room)
+        {
+            return groups.Where(x => x.Room == room).ToList();
+        }
+
+        public List<CourseGroup> GetAllGroup()
+        {
+            return groups;
+        }
+
+    }
+}
