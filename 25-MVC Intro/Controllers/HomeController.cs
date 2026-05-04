f21fbdd4f315e40ef9409b961aa972cdfae9b536
zeka <zeka.huseynliwork@gmail.com>
2026-05-05 01:08:26 +0400

    task25

---
+++ 25-MVC Intro/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using _25_MVC_Intro.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _25_MVC_Intro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
